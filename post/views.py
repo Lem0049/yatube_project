2f6ec014c5db537bb1b72664ff6834605898ffa9
Алексей Ступенков <lem0049@gmail.com>
2022-02-05 13:52:45 +0300

    создание приложения post

--- post/views.py
+++ post/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Импортируем загрузчик.
+from django.template import loader
+
 
 # Create your views here.
+
+
+def index(request):
+    template = 'templates/posts/index.html'
+    return render(request, template)
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Group posts{slug}')
